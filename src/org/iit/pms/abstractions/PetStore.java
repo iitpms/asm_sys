e4e15ab6375b358e7a9e19c5f0a5373258d5855e
logicalforhad <forhad.univdhaka@gmail.com>
2014-04-05 18:44:13 +0600

    Added PetStore Class, that implements observable.

--- src/org/iit/pms/abstractions/PetStore.java
+++ src/org/iit/pms/abstractions/PetStore.java
@@ -1,13 +1,67 @@
 package org.iit.pms.abstractions;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Store {
-	private String Name;
-	private String Country;
-	private String Address;
-	private boolean isGivingDiscount;
+public class PetStore implements Observable {
+	private String name;
+	private String address;
 	private double discount;
-	private ArrayList<Animal> animalList;
+	private ArrayList<Subscriber> subscriberList;
 
+	public PetStore() {
+		subscriberList = new ArrayList<>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+		notifySubscribers(subscriberList);
+
+	}
+
+	@Override
+	public void subscribe(Subscriber subscriber) {
+		if (!subscriberList.contains(subscriber)) {
+			subscriberList.add(subscriber);
+		} else {
+			return;
+		}
+	}
+
+	@Override
+	public void unSubscribe(Subscriber subscriber) {
+		if (subscriberList.contains(subscriber)) {
+			subscriberList.remove(subscriber);
+		} else {
+			return;
+		}
+
+	}
+
+	@Override
+	public void notifySubscribers(List<Subscriber> subscriberList) {
+		for (Subscriber subscriber : subscriberList) {
+			subscriber.getDiscountMessage(getDiscount());
+		}
+	}
 }
