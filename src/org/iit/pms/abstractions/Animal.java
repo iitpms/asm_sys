45550e72f376ce8490f97e7848f3c61606dedce6
Amit Seal Ami <lordamit@gmail.com>
2014-04-05 16:11:40 +0600

    initial commit, just setting up things.

--- src/org/iit/pms/abstractions/Animal.java
+++ src/org/iit/pms/abstractions/Animal.java
@@ -3,5 +3,8 @@ package org.iit.pms.abstractions;
 public abstract class Animal {
 	public int age; 
 	public String name;
+	public boolean isAvailable;
+	public String typeName;
+	public int price;
 	
 } 
--- src/org/iit/pms/abstractions/Store.java
+++ src/org/iit/pms/abstractions/Store.java
@@ -1,5 +1,9 @@
 package org.iit.pms.abstractions;
 
 public abstract class Store {
-
+	public String Name;
+	public String Country;
+	public String Address;
+	public boolean isGivingDiscount;
+	public Animal[] animals;
 }
