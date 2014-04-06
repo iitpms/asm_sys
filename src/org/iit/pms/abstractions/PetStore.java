1164559fdacdb307dc8a2505c655c012c98d4ec7
iftekhar-ahmed <bit0220iftekhar@gmail.com>
2014-04-06 21:52:14 +0600

    Add pet counter

--- src/org/iit/pms/abstractions/PetStore.java
+++ src/org/iit/pms/abstractions/PetStore.java
@@ -1,18 +1,24 @@
 package org.iit.pms.abstractions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import org.iit.pms.builder.AnimalBuilder;
+import org.iit.pms.helper.AnimalType;
 
 public class PetStore implements Observable {
 	private String name;
 	private String address;
 	private double discount;
+	private HashMap<AnimalType, Integer> animalCountMap;
 	private List<Animal> animals;
 	private ArrayList<Subscriber> subscriberList;
 
 	public PetStore(String address, String name) {
 		this.address = address;
 		this.name = name;
+		animalCountMap = new HashMap<>();
 		subscriberList = new ArrayList<>();
 		animals = new ArrayList<>();
 	}
@@ -36,6 +42,12 @@ public class PetStore implements Observable {
 
 	public void addAnimal(Animal animal) {
 		this.animals.add(animal);
+		if(!animalCountMap.containsKey(animal.type))
+			animalCountMap.put(animal.type, 1);
+		else {
+			int current_count = animalCountMap.get(animal.type);
+			animalCountMap.put(animal.type, current_count + 1);
+		}
 	}
 
 	public Animal search(String name) throws NullPointerException {
@@ -44,6 +56,12 @@ public class PetStore implements Observable {
 				return animal;
 		}
 		return null;
+	}
+	
+	public void printCount() {
+		for(AnimalType animType : animalCountMap.keySet()) {
+			System.out.println(animType.toString() + ":" + animalCountMap.get(animType));
+		}
 	}
 
 	@Override
@@ -71,4 +89,14 @@ public class PetStore implements Observable {
 			subscriber.getDiscountMessage(getDiscount());
 		}
 	}
+	
+//	public static void main(String[] args) {
+//		PetStore ps = new PetStore("Dhaka", "MyPetty");
+//		Animal hati = new Animal(new AnimalBuilder("Hati", 10, AnimalType.EarthAnimal, 1000f, true, false, true));
+//		ps.addAnimal(hati);
+//		Animal tortoise = new Animal(new AnimalBuilder("Tortoise", 10, AnimalType.WaterAnimal, 1000f, true, false, true));
+//		ps.addAnimal(tortoise);
+//		ps.addAnimal(tortoise);
+//		ps.printCount();
+//	}
 }
