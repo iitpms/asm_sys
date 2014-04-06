b09ada6b350f322d297810bf998e6872fe422fc0
Rayhan Hossain <hossain.rayhan@outlook.com>
2014-04-06 19:45:08 +0600

    A package named org.iit.pms.builder is added. AnimalBuilder.java added. Builder Pattern Applied & Animal.java is updated.
    Signed-off-by: Rayhan Hossain <hossain.rayhan@outlook.com>

--- src/org/iit/pms/abstractions/Animal.java
+++ src/org/iit/pms/abstractions/Animal.java
@@ -1,8 +1,9 @@
 package org.iit.pms.abstractions;
 
+import org.iit.pms.builder.AnimalBuilder;
 import org.iit.pms.helper.AnimalType;
 
-public abstract class Animal {
+public class Animal {
 	public String name;
 	public int age; 
 	public AnimalType type;
@@ -11,47 +12,14 @@ public abstract class Animal {
 	public boolean isSick;
 	public boolean isDiscountApplicable;
 	
-	protected String getName() {
-		return name;
-	}
-	protected void setName(String name) {
-		this.name = name;
-	}
-	protected int getAge() {
-		return age;
-	}
-	protected void setAge(int age) {
-		this.age = age;
-	}
-	protected AnimalType getType() {
-		return type;
-	}
-	protected void setType(AnimalType type) {
-		this.type = type;
-	}
-	protected float getPrice() {
-		return price;
-	}
-	protected void setPrice(float price) {
-		this.price = price;
-	}
-	protected boolean isAvailable() {
-		return isAvailable;
-	}
-	protected void setAvailable(boolean isAvailable) {
-		this.isAvailable = isAvailable;
-	}
-	protected boolean isSick() {
-		return isSick;
-	}
-	protected void setSick(boolean isSick) {
-		this.isSick = isSick;
-	}
-	protected boolean isDiscountApplicable() {
-		return isDiscountApplicable;
-	}
-	protected void setDiscountApplicable(boolean isDiscountApplicable) {
-		this.isDiscountApplicable = isDiscountApplicable;
+	public Animal(AnimalBuilder animalBuilder){
+		this.name = animalBuilder.name;
+		this.age = animalBuilder.age;
+		this.type = animalBuilder.type;
+		this.price = animalBuilder.price;
+		this.isAvailable = animalBuilder.isAvailable;
+		this.isSick = animalBuilder.isSick;
+		this.isDiscountApplicable = animalBuilder.isDiscountApplicable;
 	}
 	
 } 
---
+++ src/org/iit/pms/builder/AnimalBuilder.java
@@ -0,0 +1,29 @@
+package org.iit.pms.builder;
+
+import org.iit.pms.abstractions.Animal;
+import org.iit.pms.helper.AnimalType;
+
+public class AnimalBuilder {
+	public String name;
+	public int age; 
+	public AnimalType type;
+	public float price;
+	public boolean isAvailable;
+	public boolean isSick;
+	public boolean isDiscountApplicable;
+	
+	public AnimalBuilder(String name, int age, AnimalType type, float price, boolean isAvailable, boolean isSick, boolean isDiscountApplicable){
+		this.name = name;
+		this.age = age;
+		this.type = type;
+		this.price = price;
+		this.isAvailable = isAvailable;
+		this.isSick = isSick;
+		this.isDiscountApplicable = isDiscountApplicable;
+	}
+	
+	public Animal build(){
+		return new Animal(this);
+	}
+	
+}
